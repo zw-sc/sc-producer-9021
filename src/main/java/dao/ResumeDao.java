43a4cf6ddfea5425f6db2b3c691700880bad7353
zhaowei201407@163.com <zw134531>
2022-05-01 10:29:53 +0800

    修正配置参数，并启动eureka服务

--- src/main/java/dao/ResumeDao.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pojo.Resume;
-
-public interface ResumeDao extends JpaRepository<Resume, Long> {
-}
--- src/main/java/service/ResumeService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-
-import pojo.Resume;
-
-public interface ResumeService {
-
-    Resume findDefaultResumeByUserId(Long userId);
-}
